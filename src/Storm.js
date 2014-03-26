674b72588a65a55357948690763bd8799b3a4e3a
Andrew C. Dvorak <acdvorak@gmail.com>
2014-03-26 11:10:16 -0500

    Removed unmaintained Storm.VERSION property

--- src/Storm.js
+++ src/Storm.js
@@ -3,7 +3,6 @@ var previousStorm = root.Storm,
 	// Define Storm
 	Storm = root.Storm = {
 		name: 'StormJS',
-		VERSION: '0.0.1',
 		ajax: root.$ || { ajax: function() { console.error(_errorMessage('Storm.ajax', 'NYI')); } },
 		$: root.$ || function() { console.error(_errorMessage('Storm.$', 'NYI')); }
 	};
