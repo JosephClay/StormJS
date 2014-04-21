931d1481007af59af758ef39a17ce1fc47e7865e
Joseph Clay <testingriven@gmail.com>
2014-04-21 01:08:34 -0500

    Rework linenumber script

--- doc/scripts/linenumber.js
+++ doc/scripts/linenumber.js
@@ -1,25 +1,21 @@
 /*global document */
-(function() {
-    var source = document.getElementsByClassName('prettyprint source linenums');
-    var i = 0;
-    var lineNumber = 0;
-    var lineId;
-    var lines;
-    var totalLines;
-    var anchorHash;
+(function(document) {
+	var source = document.getElementById('Source');
+	if (!source) { return; }
 
-    if (source && source[0]) {
-        anchorHash = document.location.hash.substring(1);
-        lines = source[0].getElementsByTagName('li');
-        totalLines = lines.length;
+	var anchorHash = document.location.hash.substr(1),
+		lines = source.getElementsByTagName('li'),
+		idx = 0,
+		totalLines = lines.length,
+		lineNumber = 0,
+		lineId;
 
-        for (; i < totalLines; i++) {
-            lineNumber++;
-            lineId = 'line' + lineNumber;
-            lines[i].id = lineId;
-            if (lineId === anchorHash) {
-                lines[i].className += ' selected';
-            }
-        }
-    }
-})();
+	for (; idx < totalLines; idx++) {
+		lineNumber++;
+		lineId = 'line' + lineNumber;
+		lines[idx].id = lineId;
+		if (lineId === anchorHash) {
+			lines[idx].className += ' selected';
+		}
+	}
+}(document));
