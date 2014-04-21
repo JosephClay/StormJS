e336709fe17a8d41432a9ac068d2121b2b5de057
Joseph Clay <testingriven@gmail.com>
2014-04-21 01:08:51 -0500

    Gruntfile updates and add doc build

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,15 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-rename');
+	grunt.loadNpmTasks('grunt-contrib-rename');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-mkdir');
+	grunt.loadNpmTasks('grunt-exec');
+
+	var libs = {
+		signal: 'src/libs/Signal.js'
+	};
 
 	var srcFiles = [
 		'src/Intro.js',
@@ -56,17 +64,44 @@ module.exports = function(grunt) {
 		directories: {
 			dist: 'dist/<%= pkg.version %>'
 		},
+		mkdir: {
+			all: {
+				options: {
+					create: ['<%= directories.dist %>']
+				}
+			}
+		},
 		filenames: {
 			full: '<%= pkg.name %>-<%= pkg.version %>.js',
 			minified: '<%= pkg.name %>-<%= pkg.version %>.min.js',
-			sourcemap: '<%= pkg.name %>-<%= pkg.version %>.min.map'
+			sourcemap: '<%= pkg.name %>-<%= pkg.version %>.min.map',
+
+			rootfull: '<%= pkg.name %>.js',
+			rootminified: '<%= pkg.name %>.min.js',
+			rootsourcemap: '<%= pkg.name %>.min.map',
+
+			withsignal: '<%= pkg.name %>-signal.js',
+			withsignalminified: '<%= pkg.name %>-signal.min.js'
 		},
 		clean: {
-			files: [ '<%= directories.dist %>', '<%= filenames.full %>', '<%= filenames.minified %>', '<%= filenames.sourcemap %>' ]
+			files: [
+				'<%= directories.dist %>/*',
+
+				'<%= filenames.full %>',
+				'<%= filenames.minified %>',
+				'<%= filenames.sourcemap %>',
+				
+				'<%= filenames.rootfull %>',
+				'<%= filenames.rootminified %>',
+				'<%= filenames.rootsourcemap %>',
+				
+				'<%= filenames.withsignal %>',
+				'<%= filenames.withsignalminified %>'
+			]
 		},
 		jshint: {
-			beforeconcat: [ 'src/**/*.js' ],
-			afterconcat: [ '<%= concat.dist.dest %>' ],
+			before: [ 'src/**/*.js' ],
+			after: [ '<%= filenames.full %>' ],
 			options: {
 				'-W093': true, // Disable "Did you mean to return a conditional instead of an assignment?" warning
 				ignores: [ 'src/Intro.js', 'src/Outro.js' ]
@@ -76,41 +111,144 @@ module.exports = function(grunt) {
 			options: {
 				banner: '<%= banner %>',
 				stripBanners: true,
-				separator: '\n\n//----\n\n'
+				separator: '\n\n//----\n\n',
 			},
 			dist: {
-				src: srcFiles,
-				dest: '<%= filenames.full %>'
+				files: {
+					'<%= filenames.full %>': srcFiles,
+					'<%= filenames.rootfull %>': srcFiles,
+					'<%= filenames.withsignal %>': [libs.signal].concat(srcFiles)
+				},
+				nonull: true
 			}
 		},
 		uglify: {
-			options: {
-				// Grunt-contrib-uglify does not support adding a banner alongside sourcemaps. Add the banner to your unminified source and then uglify.
-				// banner: '<%= banner %>',
-
-				sourceMap: '<%= filenames.sourcemap %>'
+			basic: {
+				options: {
+					sourceMap: '<%= filenames.sourcemap %>'
+				},
+				files: {
+					'<%= filenames.minified %>': '<%= filenames.full %>'
+				}
 			},
-			dist: {
-				src: '<%= filenames.full %>',
-				dest: '<%= filenames.minified %>'
+			root: {
+				options: {
+					sourceMap: '<%= filenames.rootsourcemap %>'
+				},
+				files: {
+					'<%= filenames.rootminified %>': '<%= filenames.rootfull %>',
+					'<%= filenames.withsignalminified %>': '<%= filenames.withsignal %>'
+				}
+			}
+		},
+		compress: {
+			basic: {
+				options: {
+					mode: 'gzip'
+				},
+				expand: true,
+				src: ['<%= filenames.rootminified %>' ],
+				dest: '<%= directories.dist %>',
+				ext: '.gz.js'
+			},
+			pkged: {
+				options: {
+					mode: 'gzip'
+				},
+				expand: true,
+				src: ['<%= filenames.withsignalminified %>' ],
+				dest: '<%= directories.dist %>',
+				ext: '.gz.js'
 			}
 		},
 		rename: {
-			full: {
-				src: '<%= filenames.full %>',
-				dest: '<%= directories.dist %>/<%= filenames.full %>'
-			},
-			minified: {
-				src: '<%= filenames.minified %>',
-				dest: '<%= directories.dist %>/<%= filenames.minified %>'
-			},
-			sourcemap: {
-				src: '<%= filenames.sourcemap %>',
-				dest: '<%= directories.dist %>/<%= filenames.sourcemap %>'
+			basic: {
+				files: [
+					{
+						// full
+						src: '<%= filenames.full %>',
+						dest: '<%= directories.dist %>/<%= filenames.full %>'
+					},
+					{
+						// minified
+						src: '<%= filenames.minified %>',
+						dest: '<%= directories.dist %>/<%= filenames.minified %>'
+					},
+					{
+						// sourcemap
+						src: '<%= filenames.sourcemap %>',
+						dest: '<%= directories.dist %>/<%= filenames.sourcemap %>'
+					}
+				]
+			}
+		},
+		exec: {
+			doc: {
+				command: 'jsdoc -c jsdoc.conf.json'
+			}
+		},
+		copy: {
+			doc: {
+				files: [
+					{
+						expand: true,
+						src: [
+							'<%= filenames.rootfull %>',
+							'<%= filenames.rootminified %>',
+							'<%= filenames.withsignal %>',
+							'<%= filenames.withsignalminified %>'
+						],
+						dest: 'doc/scripts',
+						filter: 'isFile'
+					}
+				]
 			}
 		}
 	});
 
-	grunt.registerTask('default', ['clean', 'jshint:beforeconcat', 'concat', 'jshint:afterconcat', 'uglify', 'rename']);
+	grunt.registerTask('default', [
+		'mkdir',
+		'clean',
+
+		'jshint:before',
+		'concat',
+		'jshint:after',
+
+		'uglify:basic',
+		'uglify:root',
+		'compress:basic',
+		'compress:pkged',
+
+		'rename:basic'
+	]);
+
+	grunt.registerTask('doc', function() {
+		var pkg = grunt.file.readJSON('package.json'),
+			config = {
+				source: {
+					include: [
+						"StormJS.js"
+					]
+				},
+				opts: {
+					pkg: {
+						name: pkg.name,
+						title: pkg.title,
+						version: pkg.version,
+						repo: pkg.repository.url
+					},
+					encoding: "utf8",
+					template: "templates/default",
+					destination: "doc"
+				},
+				tags: {
+					allowUnknownTags: true
+				}
+			};
+
+		grunt.file.write('jsdoc.conf.json', JSON.stringify(config));
+
+		grunt.task.run(['exec:doc', 'copy:doc']);
+	});
 
 };
